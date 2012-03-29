69351405a7d30c8dae51eb6619f820650ef0d817
Wyatt Allen <wyatt.allen0@gmail.com>
2012-03-29 14:31:03 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var app = {
+    roles:[ 
+        "Designer", 
+        "Gentleman",
+        "Innovator", 
+        "Nice Guy", 
+        "Problem Solver",
+        "Creative Spirit", 
+        "Haskeller",
+        "RSS-Feed Addict",
+        "Bookworm",
+        "JavaScript Ninja",
+        "Leftie"
+    ],
+    roleIdx:0,
+    roleInterval:2000,
+    
+    setupRoleSpinner:function() {
+        app.roleIdx = (app.roleIdx + 1) % app.roles.length;
+        
+        $("<div/>")
+            .text(app.roles[app.roleIdx])
+            .appendTo("#spinning");
+            
+        $("#spinning")
+            .animate(
+                { scrollTop:$("#spinning > div:eq(1)").offset().top }, 
+                1000,
+                function() {
+                    $("#spinning > div:eq(0)")
+                        .remove();
+                        
+                    setTimeout(app.setupRoleSpinner, app.roleInterval);
+                }
+            );
+    }
+};
+
+$(function() {
+    setTimeout(app.setupRoleSpinner, app.roleInterval * 2);
+});
